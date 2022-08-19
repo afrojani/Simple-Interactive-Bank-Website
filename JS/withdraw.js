97909b72f97f31c24389061124edbe038ef4c9d7
afrojani <sultana.ani2019@gmail.com>
2022-08-19 17:33:23 -0500

    withdraw section edited

--- JS/withdraw.js
+++ JS/withdraw.js
@@ -3,24 +3,34 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
 
     const newWithdrawAmount = withdrawField.value;
 
+    withdrawField.value = '';
+
+
+    if (isNaN(parseFloat(newWithdrawAmount))) {
+        alert('Please provide a valid number');
+        return;
+    }
+
     const withdrawTotal = document.getElementById('withdraw-total');
 
     const previousWithdrawTotal = withdrawTotal.innerText;
+
+
+
+
+
+    const balanceTotal = document.getElementById('balance-total');
+    const currentBalance = balanceTotal.innerText;
+
+    if (parseFloat(newWithdrawAmount) > parseFloat(currentBalance)) {
+        alert('Baap er bank e eto taka nei.');
+        return;
+    }
 
     const newWithdrawTotal = parseFloat(previousWithdrawTotal) + parseFloat(newWithdrawAmount);
 
     withdrawTotal.innerText = newWithdrawTotal;
 
-    const balanceTotal = document.getElementById('balance-total');
-    const currentBalance = balanceTotal.innerText;
     const newBalance = parseFloat(currentBalance) - parseFloat(newWithdrawAmount);
     balanceTotal.innerText = newBalance;
-
-
-    withdrawField.value = '';
-
-
-
-
-
 })
